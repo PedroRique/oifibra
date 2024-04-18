26efb853457c88771f23a58308e19a13281eb724
Pedro Rique <pedroh.rique@hotmail.com>
2024-04-18 16:10:28 -0300

    feat: update add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Oi Fibra",
@@ -15,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
